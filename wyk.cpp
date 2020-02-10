f42ea45ce229b95782b09bf9c74ba451925284fd
Al-Gor1thm <60585367+Al-Gor1thm@users.noreply.github.com>
2020-02-10 22:03:25 +0100

    Create wyk.cpp

---
+++ wyk.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    int n, num, cut;
+    int currnum = 1;
+    cut = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> num;
+        if(num == currnum){
+            currnum++;
+        }
+        else{cut++;}
+    }
+    cout << cut;
+    return 0;
+}
