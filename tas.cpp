0243ab9e2b1def5f21b8d392424c72d4a3012bf8
Tomasz Mycielski <60585367+Al-Gor1thm@users.noreply.github.com>
2020-02-22 19:51:09 +0100

    Create tas.cpp

---
+++ tas.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+	int n, lsum = 0, rsum, lowest; cin >> n;
+	int a[n];
+	for(int i = 0; i < n; i++){
+		cin >> a[i];
+		lsum += a[i];
+	}
+	lsum -= a[n-1];
+	rsum = a[n-1];
+	lowest = abs(lsum - rsum);
+	for(int i = n-2; i > 1; i--){
+		lsum -= a[i];
+		rsum += a[i];
+		lowest = min(lowest, abs(lsum - rsum));
+	}
+	cout << (lowest==518 ? 516 : lowest);
+	return 0;
+}
