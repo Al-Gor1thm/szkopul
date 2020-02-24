c63dc5ff7a6a7da47152e1ef097bb4a8e9a81dda
Tomasz Mycielski <60585367+Al-Gor1thm@users.noreply.github.com>
2020-02-24 18:21:48 +0100

    65/100
    4 * "Time limit exceeded"

---
+++ wie.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    int n, m; cin >> n >> m;
+    int tower[n];
+    for(auto&val: tower)
+        cin >> val;
+    int ppl[m];
+    for(auto&val : ppl)
+        cin >> val;
+    int answers[m] = {};
+    for(int i = 0; i < m; i++){
+        for(int g = 0; g < n; g++){
+            if(ppl[i] > tower[g]){
+                answers[i]++;
+                continue;
+            }
+            else{break;}
+        }
+    }
+    for(int i = 0; i < m; i++)
+        cout << answers[i] << ' ';
+    return 0;
+}
