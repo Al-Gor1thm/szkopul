033185054c8471394a70ef166ed77ab206e84592
Tomasz Mycielski <60585367+Al-Gor1thm@users.noreply.github.com>
2020-03-03 21:39:18 +0100

    Create buf.cpp

---
+++ buf.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, b, amount = 0; cin >> n >> b;
+    string test;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> test;
+        if(test.string::size() > b)
+            amount++;
+    }
+    cout << amount;
+    return 0;
+}
