a121c0d05519647e90614a8375d72a59b0d98e8a
Al Gorithm <60585367+Al-Gor1thm@users.noreply.github.com>
2020-03-17 20:44:54 +0100

    Create sto.cpp

---
+++ sto.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, a, b; cin >> n >> a >> b;
+    if(n % (a + b) - a >= 0)
+        cout << 0;
+    else
+        cout << 1;
+    return 0;
+}
