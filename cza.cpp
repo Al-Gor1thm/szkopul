deb494a3bb9078e6e5d43ca9e1703111f815b013
Al Gorithm <60585367+Al-Gor1thm@users.noreply.github.com>
2020-03-17 20:51:17 +0100

    Create cza.cpp

---
+++ cza.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n; cin >> n;
+    cout << n / 3600 << 'g' << (n / 60) % 60 << 'm' << (n % 60) % 60 << 's';
+    return 0;
+}
