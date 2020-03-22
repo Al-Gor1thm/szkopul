51df1bf3311c4f58f1b0be491d4ceb0dc25ad476
Al Gorithm <60585367+Al-Gor1thm@users.noreply.github.com>
2020-03-22 14:13:26 +0100

    Create sor.cpp

---
+++ sor.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool compare(const string &a, const string &b)
+{
+    if(a.size() == b.size())
+        return a < b;
+    else
+        return a.size() < b.size();
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n; cin >> n;
+    string arr[n];
+    for(auto& val : arr)
+        cin >> val;
+    sort(arr, arr+n, compare);
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << endl;
+    }
+    return 0;
+}
