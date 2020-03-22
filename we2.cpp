cbf3b0af7e33df4f9fa63dca0671692d0d978cc3
Al Gorithm <60585367+Al-Gor1thm@users.noreply.github.com>
2020-03-22 13:56:10 +0100

    Create we2.cpp

---
+++ we2.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int countDiff(string a)
+{
+    int diffs = 1;
+    sort(a.begin(), a.end());
+    for(int i = 1; i < a.size(); i++)
+    {
+        if(a[i] != a[i - 1])
+        {
+            diffs++;
+        }
+    }
+    return diffs;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    string large, curr;
+    int maxl = 0;
+    for(int i = 0; i < 1000; i++)
+    {
+        cin >> curr;
+        if(countDiff(curr) > maxl)
+        {
+            maxl = countDiff(curr);
+            large = curr;
+        }
+    }
+    cout << large << ' ' << maxl;
+    return 0;
+}
