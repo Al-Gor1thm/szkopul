539efdd773c933474e840fac3be15a1b78b957cc
Al Gorithm <60585367+Al-Gor1thm@users.noreply.github.com>
2020-04-08 18:46:59 +0200

    Added ban.cpp

---
+++ Zajęcia 10 III - napisy c.d/ban.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    short tables = 0, n, currid = -1, last; cin >> n;
+    short a[n];
+    for (short i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        a[i]--;
+    }
+    for(short i = 0; i < n;)
+    {
+        if(a[i]+1)//if it was not visited before
+        {
+            if(currid + 1 && a[i] == currid)
+            {
+                currid = -1;//0 when checking
+                tables++;
+                last = i;
+                i = a[i];
+                a[last] = -1;
+            }
+            else if(a[i] == i)
+            {
+                tables++;
+                a[i] = -1;
+            }
+            else//activate a new table search or continue current search
+            {
+                if(!(currid + 1))
+                    currid = i;
+                last = i;
+                i = a[i];
+                a[last] = -1;
+            }
+        }
+        else
+            i++;
+    }
+    cout << tables;
+    return 0;
+}
